04faddc3fd145c2e19457bbfeab3a7662f71c9c8
Amkam <amaduskamara@gmail.com>
2023-07-27 08:15:18 +0000

    Refresher on old react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>
+            <Link to="/">King Router</Link>
+          </h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
